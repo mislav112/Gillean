e791784340f19cf8b096fdc5bcf937acc1fecb53
mislav112 <mislav.javor@gmail.com>
2014-07-26 23:14:48 +0200

    Path following scripts
    Scripts for path following

---
+++ Gillean/Assets/Scripts/Path2D.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
+
+public class Path2D : MonoBehaviour
+{
+
+    public Transform[] Points;
+
+
+    public IEnumerator<Transform> GetPathPointsEnumerator()
+    {
+        var direction = 1;
+        var index = 0;
+
+        if (Points == null || Points.Length < 1)
+            yield break;
+
+        while (true)
+        {
+            yield return Points[index];
+
+            if (index <= 0)
+                direction = 1;
+            else if (index >= (Points.Length - 1))
+                direction = -1;
+
+            index += direction;
+        }
+
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (Points.Length < 2)
+        {
+            return;
+        }
+
+        for (var i = 1; i < Points.Length ; i++)
+        {
+            Gizmos.DrawLine(Points[i - 1].position, Points[i].position);
+        }
+
+    }    
+}
---
+++ Gillean/Assets/Scripts/PathFollower.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathFollower : MonoBehaviour
+{
+    public enum MovementType
+    {
+        MoveToward,
+        Lerp
+    }
+
+    public MovementType Type = MovementType.MoveToward;
+    private IEnumerator<Transform> _currentPoint;
+    public Path2D SelectedPath;
+    public float Speed = 1;
+    public float MaxDistanceToRegisterTouch = .1f;
+    public void Start()
+    {
+        if (SelectedPath == null)
+        {
+            Debug.Log("Path2D is null", gameObject);
+            return;
+        }
+        _currentPoint = SelectedPath.GetPathPointsEnumerator();
+        _currentPoint.MoveNext();
+
+        if (_currentPoint.Current == null)
+            return;
+
+        transform.position = _currentPoint.Current.position;
+    }
+
+    public void Update()
+    {
+        if(_currentPoint == null || _currentPoint.Current == null)
+            return;
+        
+        
+        if (Type == MovementType.MoveToward)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _currentPoint.Current.position,
+                Time.deltaTime * Speed);
+        }
+        if (Type == MovementType.Lerp)
+        {
+            transform.position = Vector3.Lerp(transform.position, _currentPoint.Current.position,
+                Time.deltaTime * Speed);
+        }
+
+        var distanceSquared = (transform.position - _currentPoint.Current.position).sqrMagnitude;
+        if (distanceSquared < MaxDistanceToRegisterTouch*MaxDistanceToRegisterTouch)
+        {
+            _currentPoint.MoveNext();
+        }
+    }
+
+}
